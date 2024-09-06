6108a92b2ce26d90931c2b9708540eb6ef38d05f
RYC0208 <ryc0208@naver.com>
2024-09-06 18:02:41 +0900

    비동기2 - promise all, async await

--- async-app/src/App.jsx
+++ async-app/src/App.jsx
@@ -1,39 +1,81 @@
-// import React, { useEffect, useState } from "react";
+// // import React, { useEffect, useState } from "react";
+
+// // const App = () => {
+// //   const [message, setMessage] = useState("타이머 시작");
+
+// //   useEffect(() => {
+// //     const delay = function (ms) {
+// //       const promise = new Promise((resolve) => {
+// //         setTimeout(resolve, ms);
+// //       });
+
+// //       return promise;
+// //     };
+
+// //     delay(2000).then(function () {
+// //       setMessage("2초 후 메세지가 변경되었씁니다.");
+// //     });
+// //   }, []);
+// //   return (
+// //     <div>
+// //       <h1>{message}</h1>
+// //     </div>
+// //   );
+// // };
+
+// // export default App;
+
+// import React, { useEffect } from "react";
 
 // const App = () => {
-//   const [message, setMessage] = useState("타이머 시작");
-
 //   useEffect(() => {
-//     const delay = function (ms) {
-//       const promise = new Promise((resolve) => {
-//         setTimeout(resolve, ms);
-//       });
-
-//       return promise;
-//     };
-
-//     delay(2000).then(function () {
-//       setMessage("2초 후 메세지가 변경되었씁니다.");
-//     });
-//   }, []);
-//   return (
-//     <div>
-//       <h1>{message}</h1>
-//     </div>
-//   );
+//     // fetch("https://jsonplaceholder.typicode.com/posts/1")
+//     //   .then((response) => response.json())
+//     //   .then((json) => console.log(json));
+//     Promise.all([
+//       fetch("https://jsonplaceholder.typicode.com/posts/1").then((response) =>
+//         response.json()
+//       ),
+//       fetch("https://jsonplaceholder.typicode.com/posts/1").then((response) =>
+//         response.json()
+//       ),
+//     ]);
+//   }).then(function ([response1, response2]){
+//     console.log("response=>1", response1)
+//     console.log("response=>2", response2)
+//   });
+//   return <div></div>;
 // };
 
 // export default App;
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [post, setPost] = useState(null);
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-  });
-  return <div></div>;
+    const fetchPost = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts/1"
+        );
+        const data = await response.json();
+        setPost(data);
+      } catch (error) {
+        console.error("Error =>", error);
+      }
+    };
+    fetchPost();
+  }, []);
+
+  console.log("post", post);
+  return (
+    <div>
+      <h3>async / await 연습</h3>
+      {post ? <div>{post.title}</div> : <div>loading...</div>}
+    </div>
+  );
 };
 
 export default App;
